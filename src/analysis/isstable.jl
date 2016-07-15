708d60992df396cb91ae385013cc3190f2d1d37d
Niklas Everitt <neveritt@kth.se>
2016-07-15 12:28:03 +0200

    added isstable method

--- src/analysis/isstable.jl
+++ src/analysis/isstable.jl
@@ -1,6 +1,6 @@
-isstable{T<:ControlCore.DSiso}(s::T) = maximum(abs([pole(sys), 0])) >= 1
+isstable{T<:ControlCore.DSiso}(s::T) = maximum(abs([poles(s); 0])) < 1
 
-isstable{T<:ControlCore.CSiso}(s::T) = maximum(real([pole(sys), 0])) >= 0
+isstable{T<:ControlCore.CSiso}(s::T) = maximum(real([poles(s); 0])) < 0
 
 isstable{T<:ControlCore.MimoSystem}(s::T) = map(isstable, getmatrix(s))
 # TODO: How to handle pole-zero cancellation
