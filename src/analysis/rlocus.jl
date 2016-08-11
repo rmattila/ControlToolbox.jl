b49de795aa0bd7fb031ccc0802c0f141f04dc4b7
Niklas Everitt <neveritt@kth.se>
2016-08-11 17:05:02 +0200

    updated rlocus

--- src/analysis/rlocus.jl
+++ src/analysis/rlocus.jl
@@ -1,32 +1,26 @@
 """
-    `rlocus(sys, [K])`
-    Computes the root locus of `sys`.
-    
-    Returns the closed-loop system
-        - poles at `K`, if `K` is a number
-        - pole trajectories, if `K` is vector of gains
-        - pole trajectories at evenly spaced intervals of the gain, if `K` is
-          omitted
-"""
-function rlocus(sys::ControlCore.LtiSystem)                                                                                       
-    # This should be adaptive (for a smooth plot)
-    K = collect(linspace(1,1e2))
+`rlocus(sys, [K])`
+Computes the root locus of `sys`.
 
-    return rlocus(sys, K)
+Returns the closed-loop system
+- poles at `K`, if `K` is a number
+- pole trajectories, if `K` is vector of gains
+- pole trajectories at evenly spaced intervals of the gain, if `K` is
+omitted
+"""
+function rlocus(sys::ControlCore.LtiSystem)
+  # This should be adaptive (for a smooth plot)
+  K = collect(logspace(-2,2,100))
+
+  return rlocus(sys, K)
 end
 
 function rlocus{T<:Real}(sys::ControlCore.LtiSystem, K::AbstractVector{T})
-    rlist = Tuple{AbstractVector{Complex}, Real}[]
-    for k in K
-        push!(rlist, rlocus(sys, k))
-    end
-
-    return rlist
+  plist = Matrix{eltype(poles(sys))}(length(K), length(denvec(sys))-1)
+  for idx in eachindex(K)
+    plist[idx,:] = rlocus(sys, K[idx])[1]
+  end
+  (plist, K)
 end
 
-function rlocus(sys::ControlCore.LtiSystem, K::Real)
-    plist = poles(feedback(sys,K))
-
-    return (plist, K)
-end
-
+rlocus(sys::ControlCore.LtiSystem, k::Real) =  poles(feedback(sys,k)), k
